313ee0d992ffacde8d7d0f762b866f67b7f5084a
poem <normamoras@gmail.com>
2020-03-06 09:53:56 -0500

    added prompt queries

--- index.js
+++ index.js
@@ -35,11 +35,88 @@ class Database {
     database: "tracker"
 });
 
+async function main() {
+    console.log("i am running");
+    const employeeTrackerData = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'select',
+            message: 'what would you like to do',
+            choices: [  'view all employee', 
+                        'view all employee by role',
+                        'view all employee by department',
+                        'view all employees by manager',
+                        'add employee',
+                        'remove employee',
+                        'update employee role',
+                        'update employee manager'
+                    ],    
+        }, 
+    
+    ]);
+
+    if(employeeTrackerData.select == "view all employee"){
+        let employeeTable = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(employeeTable);
+    }
+    if(employeeTrackerData.select == 'view all employee by role'){
+        let employeeRole = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(employeeRole);
+    };
+    if(employeeTrackerData.select == 'view all employee by department'){
+        let employeeDepartment = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(employeeDepartment);
+    };
+    if(employeeTrackerData.select == 'view all employees by manager'){
+        let employeeManager = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(employeeManager);
+    };
+    if(employeeTrackerData.select == 'add employee'){
+        let addEmployee = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(addEmployee);
+    };
+    if(employeeTrackerData.select == 'remove employee'){
+        let removeEmployee = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(removeEmployee);
+    };
+    if(employeeTrackerData.select == 'update employee role'){
+        let updateRole = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(updateRole);
+    };
+    if(employeeTrackerData.select == 'update employee manager'){
+        let updateManager = await db.query( `SELECT * from employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id;` )
+            console.table(updateManager);
+    };
+
+
+    
 
 
 
+    // const departmentData = await inquirer.prompt([
+    //     {
+    //         name: 'department',
+    //         type: 'input',
+    //         message: `What is the name of the department?`
+    //     }
+    // ]);
+    
+    // await db.query( "INSERT INTO Department (`name`) VALUES(?)", [departmentData.name] );   
+
+    // const employeeData = await inquirer.prompt([
+    //     {
+    //         name: 'title',
+    //         type: 'input',
+    //         message: `What is the Role Title?`
+    //     },
+    
+    // ])
+    // await db.query( "INSERT INTO Department (`name`) VALUES(?)", [departmentData.name] );   
+
+    
 
 
 
-
-module.exports = { db } 
+}
+main();
+module.exports = { db } ;
